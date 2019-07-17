51b356b8064d458b8c7a9cdb2505daad6ccb6844
Christopher <christopher.carnivale@gmail.com>
2019-07-17 17:03:20 -0400

    refactor original code

--- bowling.js
+++ bowling.js
@@ -1,32 +1,24 @@
-"use strict";
+'use strict';
 
-module.exports = function score(frames){
-    let frameArr = frames.split(" ");
-    let totalScore = 0;
+module.exports = function score(frames) {
+  return frames.split(' ').reduce((acc, frame, i, frameArr) => {
+    return acc + scoreRolls(frame, frameArr[i + 1]);
+  }, 0);
+};
 
-    frameArr.forEach((frame, i) => {
-        let frameScore = scoreFrame(frame, frameArr[i + 1]); 
-        totalScore += frameScore; 
-    })
-    
-    function scoreFrame(frame, nextFrame){
-        if (isSpare(frame[1])){
-            let bonus = scoreRoll(nextFrame[0])
-            return 10 + bonus;
-        }
-        return scoreRoll(frame[0]) + scoreRoll(frame[1]);
-    }
-
-    function scoreRoll(roll){
-        if (roll === "-"){
-            return 0;
-        }
-        return parseInt(roll);
-    }
-    function isSpare(roll){
-        if(roll === "/"){
-            return true
-        }
-    }
-    return totalScore;
+function scoreRolls(frame, nextFrame) {
+  let score = 0;
+  
+  if (frame[1] === '/') {
+    const bonus = scoreRolls(nextFrame[0]);
+    score = 10 + bonus;
+  } else{
+    score = frame.split('').reduce((acc, roll) => {
+      if (roll === '-'){
+        return acc + 0;
+      }
+      return acc + parseInt(roll); 
+    }, 0);
+  }
+  return score; 
 }
