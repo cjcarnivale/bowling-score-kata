7d35f5bfcb78be9fb6aa7e02fd8d104346197e56
Christopher <christopher.carnivale@gmail.com>
2019-07-18 19:07:17 -0400

    tests failing - find the base case

--- bowling-stack-recursion.js
+++ bowling-stack-recursion.js
@@ -2,6 +2,31 @@
 
 const Stack = require('./stack');
 
-module.exports = function(frames){
-  return 0; 
+module.exports = function main(frames){
+  let framesStack = new Stack;
+  frames.split(' ').forEach(frame => {
+    framesStack.push(frame);
+  });
+  return frameScore(framesStack);
 };
+
+function frameScore(frames){
+  //Base case
+  if(frames.isEmpty()){
+    let score = simpleScore(frames.pop());
+    return score + frameScore(frames);
+  }
+}
+
+function simpleScore(frame){ 
+  const rolls = frame.split('');
+  let rollsScore = 0; 
+  rolls.forEach(roll => {
+    if(roll === '-'){
+      rollsScore += 0; 
+    } else { 
+      rollsScore += parseInt(roll);
+    }
+  }); 
+  return rollsScore; 
+}
