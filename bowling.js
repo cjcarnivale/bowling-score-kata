2752b779edf1facf7209a0d6cf4a162917d1cfc6
Christopher <christopher.carnivale@gmail.com>
2019-07-18 11:28:23 -0400

    refactor

--- bowling.js
+++ bowling.js
@@ -9,55 +9,52 @@ module.exports = function score(frames) {
 
 function scoreFrame(frame, i, frameArr) {
   let score = 0;
-  if (frame[0] === 'X'){
+  if (frame[0] === 'X') {
     score = calculateStrike(i, frameArr);
   } else if (frame[1] === '/') {
     score = calculateSpare(i, frameArr);
-  } else{
-    score = simpleScore(frame[0]) + simpleScore(frame[1]); 
+  } else {
+    score = simpleScore(frame[0]) + simpleScore(frame[1]);
   }
-  return score;   
+  return score;
 }
 
-function calculateStrike(i, frameArr){
-  let bonus = 0; 
+function calculateStrike(i, frameArr) {
+  let bonus = 0;
   //Final frame calculation
-  if (i === 9){
-    bonus = simpleScore(frameArr[i][1]) + simpleScore(frameArr[i][2]); 
+  if (i === 9) {
+    bonus = simpleScore(frameArr[i][1]) + simpleScore(frameArr[i][2]);
   } else {
     const nextRoll = frameArr[i + 1][0];
     //Second next roll could be either in the next frame or the frame after
-    const secondNextRoll =  ((frameArr[i+1][1] ||frameArr[i+2][0]));
+    const secondNextRoll = frameArr[i + 1][1] || frameArr[i + 2][0];
     //In the case that a strike is followed by a spare, to get the value
     //of the second roll, the first roll must be passed down.
-    bonus = simpleScore(nextRoll) + simpleScore((secondNextRoll), frameArr[i + 1][0]);
+    bonus =
+      simpleScore(nextRoll) + simpleScore(secondNextRoll, frameArr[i + 1][0]);
   }
-  return 10 + bonus; 
+  return 10 + bonus;
 }
 
-function calculateSpare(i, frameArr){
-  let bonus = 0; 
+function calculateSpare(i, frameArr) {
+  let bonus = 0;
   //Final frame calculation
-  if (i === 9){
-    bonus = simpleScore(frameArr[i][2]); 
+  if (i === 9) {
+    bonus = simpleScore(frameArr[i][2]);
+  } else {
+    bonus = simpleScore(frameArr[i + 1][0]);
   }
-  else {
-    bonus = simpleScore(frameArr[i+1][0]); 
-  } 
-  return 10 + bonus; 
+  return 10 + bonus;
 }
 
-function simpleScore(roll, prevRoll){ 
-  if (roll === 'X'){
+function simpleScore(roll, prevRoll) {
+  if (roll === 'X') {
     return 10;
-  }
-
-  if (roll === '/'){
-    return  10 - parseInt(prevRoll); 
-  }
-
-  if (roll === '-'){
+  } else if (roll === '/') {
+    return 10 - parseInt(prevRoll);
+  } else if (roll === '-') {
     return 0;
+  } else {
+    return parseInt(roll);
   }
-  return parseInt(roll); 
 }
