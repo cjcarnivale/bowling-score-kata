3a38e59c33ba5b36bc46b7f79e2269fb7b5bed1f
Christopher <christopher.carnivale@gmail.com>
2019-07-22 14:54:41 -0400

    all tests passing, added comments

--- bowling-stack-recursion.js
+++ bowling-stack-recursion.js
@@ -3,14 +3,16 @@
 const Stack = require('./stack');
 
 module.exports = function main(frames){
+  //Set-up Stack
   let frameStack = new Stack;
-  if(frames[22]){
-    frames.slice(20, 22); 
-    frames[19] === 'XXX'; 
+  //3 strikes in the last frame formatting
+  if (frames[22] === 'X'){
+    frames = frames.substring(0, 18).concat('XXX'); 
   }
   frames.replace(/-/gi, '0').split(' ').forEach(frame => {
     frameStack.push(frame);
   });
+  //Call scoring function
   return gameScore(frameStack);
 };
 
@@ -21,34 +23,44 @@ function gameScore(frameStack, currentFrame = frameStack.pop(), previousFrame, n
     return 0; 
   }
 
+  //Spare
   if (currentFrame.includes('/')) {
+    //Final frame
     if (currentFrame.length === 3){
       frameScore += 10 + simpleScore(currentFrame[2]); 
     } else {
       frameScore += 10 + simpleScore(previousFrame[0]); 
     }
+  //Strike
   } else if (currentFrame.includes('X')){
+    //Final frame
     if (currentFrame.length === 3){
       frameScore += 30; 
     }
+    //Previous frame is a spare
     else if (previousFrame[1] === '/'){
       frameScore += 20; 
     } else {
+    //Previous frame is a strike, or not a spare
       frameScore += (10 + simpleScore(previousFrame[0]) + simpleScore(previousFrame[1] ? previousFrame[1] : nextPreviousFrame[0]));
-    }  
+    }
+  //Neither strike nor spare  
   } else {
     frameScore += simpleScore(currentFrame[0]) + simpleScore(currentFrame[1]); 
-  }
-
-  function simpleScore(roll){
-    if (roll === 'X'){
-      return 10;
-    }
-    return parseInt(roll); 
   }
 
   nextPreviousFrame = previousFrame; 
   previousFrame = currentFrame; 
 
+  //Recursive call
   return frameScore + gameScore(frameStack, frameStack.pop(), previousFrame, nextPreviousFrame); 
 }
+
+//Helper function to get numeric values
+function simpleScore(roll){
+  if (roll === 'X'){
+    return 10;
+  }
+  return parseInt(roll); 
+}
+
--- test/bowling.test.js
+++ test/bowling.test.js
@@ -1,45 +1,45 @@
 'use strict';
 
 // const score = require('../bowling');
-const score = require('../bowling-stack-recursion');
+const main = require('../bowling-stack-recursion');
 
 const { expect } = require('chai');
 
-describe('score', () => {
+describe('main', () => {
   it.only('should return 0 if all gutter balls rolled', () => {
-    expect(score('-- -- -- -- -- -- -- -- -- --')).to.equal(0);
+    expect(main('-- -- -- -- -- -- -- -- -- --')).to.equal(0);
   });
   it.only('should return 10 if 1 pin per frame was destroyed', () => {
-    expect(score('1- 1- 1- 1- 1- 1- 1- 1- 1- 1-')).to.equal(10);
+    expect(main('1- 1- 1- 1- 1- 1- 1- 1- 1- 1-')).to.equal(10);
   });
   it.only('should return correct score if a spare is present and next frame is gutters', () => {
-    expect(score('1/ -- -- -- -- -- -- -- -- --')).to.equal(10);
+    expect(main('1/ -- -- -- -- -- -- -- -- --')).to.equal(10);
   });
   it.only('should return correct score if a spare is present and next frame is greater than 0', () => {
-    expect(score('1/ 11 -- -- -- -- -- -- -- --')).to.equal(13);
+    expect(main('1/ 11 -- -- -- -- -- -- -- --')).to.equal(13);
   });
   it.only('should return correct score if a spare is present and next frame is a spare', () => {
-    expect(score('1/ 1/ 11 -- -- -- -- -- -- --')).to.equal(24);
+    expect(main('1/ 1/ 11 -- -- -- -- -- -- --')).to.equal(24);
   });
   it.only('should return correct score if a strike is present and next frame is gutters', () => {
-    expect(score('X -- -- -- -- -- -- -- -- --')).to.equal(10);
+    expect(main('X -- -- -- -- -- -- -- -- --')).to.equal(10);
   });
   it.only('should return correct score if a strike is present and next frame is greater than 0', () => {
-    expect(score('X 11 -- -- -- -- -- -- -- --')).to.equal(14);
+    expect(main('X 11 -- -- -- -- -- -- -- --')).to.equal(14);
   });
   it.only('should return correct score if a strike is present and next frame is a strike', () => {
-    expect(score('X X 11 -- -- -- -- -- -- --')).to.equal(35);
+    expect(main('X X 11 -- -- -- -- -- -- --')).to.equal(35);
   });
   it.only('should return correct score if a strike is present and next frame is a spare', () => {
-    expect(score('X 1/ -- -- -- -- -- -- -- --')).to.equal(30);
+    expect(main('X 1/ -- -- -- -- -- -- -- --')).to.equal(30);
   });
   it.only('should return correct score if every frame is a spare plus extra roll in final frame', () => {
-    expect(score('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5')).to.equal(150);
+    expect(main('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5')).to.equal(150);
   });
   it.only('should return correct score if every frame is a strike with an additional two strikes in the final frame', () => {
-    expect(score('X X X X X X X X X X X X')).to.equal(300);
+    expect(main('X X X X X X X X X X X X')).to.equal(300);
   });
   it.only('should return the correct score with a mix of strikes, spares, and gutters', () => {
-    expect(score('8/ 54 9- X X 5/ 53 63 9/ 9/X')).to.equal(149);
+    expect(main('8/ 54 9- X X 5/ 53 63 9/ 9/X')).to.equal(149);
   });
 });
